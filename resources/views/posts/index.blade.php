c5d3262a8b82a949801a922073f37df7c0a16935
DESKTOP-95C44TF\Fumina <agisi.haniwa@gmail.com>
2024-03-02 18:31:52 +0900

    アイコンをidごとに表示する記述追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,14 @@
     <table class='table table-hover'>
     {!! Form::open(['url' => '/post/create']) !!}
     <div class="form-group">
-    <p><a class="btn user-icon" href="/profile"><img src="" alt="アイコン"></a></p>
+    <p><a class="btn user-icon" href="/profile"><img src="images/{{ Auth::user() -> images }}" alt="アイコン"></a></p>
     {!! Form::input('text', 'newPosts', null, ['required', 'class' => 'newposts', 'placeholder' => '何をつぶやこうか...?', 'autocomplete' => 'off']) !!}
   </div>
   <button type="submit" class="postbtn"><img src="images/post.png" alt="Postする"></button>
     {!! Form::close() !!}
 @foreach ($posts as $post)
 <tr>
-  <td><a class="btn" href=""><img src="" alt="アイコン"></a></td>
+  <td><a class="btn" href=""><img src="images/{{ $post -> images }}" alt="アイコン"></a></td>
   <!-- <td>{{ $post ->user_id}}</td> -->
   <td class="username">〇〇</td>
   <td class="timeline-post">{{ $post ->posts}}</td>
@@ -19,9 +19,9 @@
 
 <!-- 編集ボタン -->
   <td>
-    <button class="btn edit-btn" data-toggle="modal" data-target="#editModal" data-postid="{{ $post->id }}"><img src="images/edit.png" alt="編集"></button>
+    <button type="submit" class="btn edit-btn" data-toggle="modal" data-target="#editModal" data-postid="{{ $post->id }}"><img src="images/edit.png" alt="編集"></button>
   </td>
-
+<!-- 削除ボタン -->
   <td><a class="btn-danger" href="/post/{{ $post->id}}/delete" onclick="return confirm('このつぶやきを削除します。よろしいでしょうか？')">
   <img src="images/trash.png" alt="削除">
 </a></td>
